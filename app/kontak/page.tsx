08c0e9b0935be1f0c10f0c5fd9ac071c73ca3906
Timothy Geraldo Manurung <geraldomanroe@gmail.com>
2025-07-29 03:16:07 +0700

    newnew2

--- app/kontak/page.tsx
+++ app/kontak/page.tsx
@@ -74,13 +74,13 @@ export default function KontakPage() {
             </div>
 
             <div className="lg:col-span-2">
-  <div className="aspect-w-16 aspect-h-9 w-full">
+  <div className="w-full h-[450px] rounded-lg overflow-hidden shadow-lg">
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15956.847958200064!2d124.48423899999999!3d0.9988985499999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x327e00757b4f718b%3A0xf9e4842a4f507b03!2sPontak%20Satu%2C%20Ranoyapo%2C%20South%20Minahasa%20Regency%2C%20North%20Sulawesi!5e0!3m2!1sen!2sid!4v1753733500140!5m2!1sen!2sid"
+      className="w-full h-full border-0"
       allowFullScreen=""
       loading="lazy"
       referrerPolicy="no-referrer-when-downgrade"
-      className="w-full h-full border-0"
     ></iframe>
   </div>
 </div>
